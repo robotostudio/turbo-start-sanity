46855f08c23532cd943fdfc748ac01bcc2ab9ea0
suraj <surajmal@robotostudio.com>
2025-09-09 17:49:50 +0530

    feat: sanity redirect function on changed published

--- apps/studio/schemaTypes/documents/index.ts
+++ apps/studio/schemaTypes/documents/index.ts
@@ -6,8 +6,9 @@ import { footer } from "./footer";
 import { homePage } from "./home-page";
 import { navbar } from "./navbar";
 import { page } from "./page";
+import { redirect } from "./redirect";
 import { settings } from "./settings";
 
 export const singletons = [homePage, blogIndex, settings, footer, navbar];
 
-export const documents = [blog, page, faq, author, ...singletons];
+export const documents = [blog, page, faq, author, ...singletons, redirect];
--- apps/studio/structure.ts
+++ apps/studio/structure.ts
@@ -10,6 +10,7 @@ import {
   PanelBottom,
   PanelBottomIcon,
   Settings2,
+  TrendingUpDown,
   User,
 } from "lucide-react";
 import type {
@@ -122,6 +123,7 @@ export const structure = (
         icon: MessageCircle,
       }),
       createList({ S, type: "author", title: "Authors", icon: User }),
+      createList({ S, type: "redirect", title: "Redirects", icon: TrendingUpDown }),
       S.divider(),
       S.listItem()
         .title("Site Configuration")
