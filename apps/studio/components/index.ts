8b1379b785b51012052580d55537423dc8819ab0
jonoroboto <jono@robotostudio.com>
2025-10-11 13:38:09 +0530

    Update VS Code settings to disable auto formatting

--- apps/studio/components/index.ts
+++ apps/studio/components/index.ts
@@ -2,18 +2,22 @@
 
 // Types
 export type { Page, TreeNode } from "../hooks/use-pages-tree";
+
 // Hooks
 export { usePagesTree } from "../hooks/use-pages-tree";
 export { useTreeExpansion } from "../hooks/use-tree-expansion";
 export { HierarchicalPagesTree } from "./hierarchical-pages-tree-refactored";
+
 // Skeleton components
 export {
   EnhancedLoadingState,
   ProgressiveSkeletonTree,
   SkeletonTree,
 } from "./skeleton-loader";
+
 // Slug components
 export { PathnameFieldComponent } from "./slug-field-component";
+
 // Sub-components
 export {
   type MenuAction,
