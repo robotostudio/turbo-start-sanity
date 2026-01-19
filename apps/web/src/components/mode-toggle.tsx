74e6a890452cb6dae8816f64c93e2b2796097a57
SameerJSRS <sameer@robotostudio.com>
2026-01-19 20:42:49 +0530

    style: change `ModeToggle` button variant from `outline` to `ghost`

--- apps/web/src/components/mode-toggle.tsx
+++ apps/web/src/components/mode-toggle.tsx
@@ -16,7 +16,7 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button className="rounded-[10px]" size="icon" variant="outline">
+        <Button className="rounded-[10px]" size="icon" variant="ghost">
           <Sun className="dark:-rotate-90 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
